1654ace3f76064945a7a62c86a581605b2dbb1bc
Ivo <79941799+ivorossi@users.noreply.github.com>
2025-02-03 17:17:56 -0300

    Update Form.tsx add reprot ip

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -21,18 +21,23 @@ const Form: React.FC<FormProps> = ({ apiEndpoint }) => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
+      const ipResponse = await fetch('https://api64.ipify.org?format=json');
+      const ipData = await ipResponse.json();
+      const userIp = ipData.ip;
+
+      const dataToSend = { ...formData, ip: userIp };
+
       const response = await fetch(apiEndpoint, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(dataToSend),
       });
 
       if (response.ok) {
         const result = await response.json();
         console.log('Server response:', result);
-        // Handle the response as needed
       } else {
         console.error('Request error:', response.statusText);
       }
@@ -44,44 +49,16 @@ const Form: React.FC<FormProps> = ({ apiEndpoint }) => {
   return (
     <FormContainer onSubmit={handleSubmit}>
       <Label htmlFor="name">Name:</Label>
-      <Input
-        type="text"
-        id="name"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        required
-      />
+      <Input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
 
       <Label htmlFor="documentNumber">Document Number:</Label>
-      <Input
-        type="text"
-        id="documentNumber"
-        name="documentNumber"
-        value={formData.documentNumber}
-        onChange={handleChange}
-        required
-      />
+      <Input type="text" id="documentNumber" name="documentNumber" value={formData.documentNumber} onChange={handleChange} required />
 
       <Label htmlFor="phone">Phone:</Label>
-      <Input
-        type="tel"
-        id="phone"
-        name="phone"
-        value={formData.phone}
-        onChange={handleChange}
-        required
-      />
+      <Input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} required />
 
       <Label htmlFor="email">Email:</Label>
-      <Input
-        type="email"
-        id="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        required
-      />
+      <Input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
 
       <Button type="submit">Submit</Button>
     </FormContainer>
